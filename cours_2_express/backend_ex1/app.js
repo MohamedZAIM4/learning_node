3a5f0ed1705c9975c6cf1cd048dd79cab0672295
unknown <mzaim@eilcalais.fr>
2025-10-17 15:58:06 +0200

    exercise one

---
+++ cours_2_express/backend_ex1/app.js
@@ -0,0 +1,37 @@
+require('dotenv').config({path: './.env'})
+
+const express = require('express'); // inclusion d'express
+const app = express(); // Instanciation d'une application express
+
+
+const path= require("path");
+
+
+// Chargement des fichiers statiques : Tjrs en 1er
+app.use(express.static(path.join(__dirname,'public')));
+
+// Set the engine
+app.set('view engine','ejs');// Définition du moteur de rendu
+app.set('views',path.join(__dirname,'views'))
+
+
+app.use((req,res,next)=> {
+    const now=new Date().toDateString();
+    console.log(`${now} : une requête ${req.method} est arrivée !`);
+    next();
+});
+
+// // Configuration de l'application : une première gestion "basique" des requêtes.
+// app.use((req, res) => {
+//     res.sendFile(path.join(__dirname,'public/pages/index.html'))
+// })
+
+// Utiliser le rendu de la vue home.ejs 
+app.use((req,res)=>{
+    res.render('pages/home.ejs', { sex:'F'});
+})
+
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`Le server écoute sur http://127.0.01:${port}/`);
+});
